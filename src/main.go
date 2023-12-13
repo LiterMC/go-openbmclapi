64be4d76542b210444a63ebeeecb0fb835b05366
zyxkad <zyxkad@gmail.com>
2023-12-12 22:12:16 -0700

    forget to uncomment again

--- src/main.go
+++ src/main.go
@@ -161,15 +161,15 @@ START:
 
 	logInfof("Starting Go-OpenBmclApi v%s", VERSION)
 
-	// {
-	// 	logInfof("Fetching file list")
-	// 	fl := cluster.GetFileList()
-	// 	if fl == nil {
-	// 		logError("Cluster filelist is nil, exit")
-	// 		os.Exit(1)
-	// 	}
-	// 	cluster.SyncFiles(fl, ctx)
-	// }
+	{
+		logInfof("Fetching file list")
+		fl := cluster.GetFileList()
+		if fl == nil {
+			logError("Cluster filelist is nil, exit")
+			os.Exit(1)
+		}
+		cluster.SyncFiles(fl, ctx)
+	}
 
 	if !cluster.Connect() {
 		os.Exit(1)

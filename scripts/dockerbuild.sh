a5571959d673f08df66d6fc53062cf08e368de96
zyxkad <zyxkad@gmail.com>
2024-05-26 08:54:30 -0600

    add license to some scripts

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+# OpenBmclAPI (Golang Edition)
+# Copyright (C) 2024 Kevin Z <zyxkad@gmail.com>
+# All rights reserved
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 PUBLIC_PREFIX=craftmine/go-openbmclapi
 BUILD_PLATFORMS=(linux/arm64 linux/amd64) #
